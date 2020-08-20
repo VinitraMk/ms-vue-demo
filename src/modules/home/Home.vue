9e9644d2b01459fef9579b64c93e5893cb993841
VinitraMk <vinitramk@gmail.com>
2020-08-20 13:31:24 +0530

    completed session-1 template

--- src/modules/home/Home.vue
+++ src/modules/home/Home.vue
@@ -14,12 +14,7 @@
                 <a v-for="(n,index) in 26" :key="'link'+index" href="#" class="px-2">{{String.fromCharCode(index+65)}}</a>
             </div>
         </div>
-
-        <div class="vd-container">
-            <div class="row">
-
-            </div>
-        </div>
+        
     </div>
 </template>
 
