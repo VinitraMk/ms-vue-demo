14b5cb33981e4b3de538af0bb6f7a05cb601ac05
VinitraMk <vinitramk@gmail.com>
2020-08-20 13:23:25 +0530

    completed starter code for session 1

---
+++ src/modules/home/Home.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="my-4">
+        <div class="container">
+            <form>
+                <div class="input-group">
+                    <input type="text" class="form-control" placeholder="Search for movies"/>
+                    <div class="input-group-append">
+                        <button type="submit" class="btn btn-primary">Search</button>
+                    </div>
+                </div>
+            </form>
+
+            <div class="d-flex justify-content-center mt-4">
+                <a v-for="(n,index) in 26" :key="'link'+index" href="#" class="px-2">{{String.fromCharCode(index+65)}}</a>
+            </div>
+        </div>
+
+        <div class="vd-container">
+            <div class="row">
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'Home'
+}
+</script>
