6841fd78fb61306e6ae82e13336a429ca29d8a9a
VinitraMk <vinitramk@gmail.com>
2020-09-28 01:13:58 +0530

    adding commit to stage session-2 files

---
+++ src/components/card/Card.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="vd-card">
+        <div class="vd-card__img">
+            <img :src="imgSrc" :alt="title"/>
+        </div>
+        <div class="vd-card__title">
+            {{title}}
+        </div>
+        <button class="btn btn-primary">View More</button>
+    </div>
+</template>
+<script>
+export default {
+    name:"Card",
+    props:{
+        title:String,
+        imgSrc:String
+    }
+}
+</script>
