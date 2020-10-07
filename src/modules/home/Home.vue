7a49fb9854df3e21be79538bc2a7c150ff29e795
VinitraMk <vinitramk@gmail.com>
2020-10-07 16:18:31 +0530

    small update

--- src/modules/home/Home.vue
+++ src/modules/home/Home.vue
@@ -9,7 +9,7 @@
                     </div>
                 </div>
                 <a href="#" class="d-block" v-on:click="toggleFilters()">Advanced Search</a>
-                <div class="d-inline-flex mt-2" v-show="showFilters">
+                <div class="d-inline-flex mt-2" v-if="showFilters">
                     <label class="mr-2">Filter By</label>
                     <select class="form-control">
                         <option>--Select--</option>
