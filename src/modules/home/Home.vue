a9e0514b4f444d88e8209cdb359b7109241c37e3
VinitraMk <vinitramk@gmail.com>
2020-10-07 11:22:35 +0530

    added collapsible class code

--- src/modules/home/Home.vue
+++ src/modules/home/Home.vue
@@ -8,8 +8,8 @@
                         <button type="submit" class="btn btn-primary" @click="searchForTitle($event)">Search</button>
                     </div>
                 </div>
-                <a href="#" class="d-block" v-on:click="toggleFilters()">Advanced Search</a>
-                <div class="d-inline-flex mt-2" v-if="showFilters">
+                <a href="#" class="d-block mb-2" v-on:click="toggleFilters()">Advanced Search</a>
+                <div class="vd-form-group vd-collapsible" :class="{active:showFilters}">
                     <label class="mr-2">Filter By</label>
                     <select class="form-control">
                         <option>--Select--</option>
